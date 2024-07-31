b2ff6c1e6fe5718565ac6f7545ca649fb74d3f8e
sleeptightAnsiC <91839286+sleeptightAnsiC@users.noreply.github.com>
2024-07-31 16:33:46 +0200

    add utils

---
+++ src/util.h
@@ -0,0 +1,25 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <assert.h>
+
+
+// TODO: disable assertions and other similar macros in shipping builds
+
+#define UTIL_ASSERT(COND) assert(COND)
+
+#define UTIL_UNREACH() do{ assert("This code should never be reached!" == 0); } while (0)
+
+#define UTIL_ARRNUM(ARR) (sizeof(ARR)/sizeof(ARR[0]))
+
+#define UTIL_DEBUGBLOCK if (true)
+
+// UTIL_STATIC_ASSERT
+#define _UTIL_STATIC_ASSERT_3(COND,MSG) typedef char STATIC_ASSERTION_##MSG[(COND) ? 1 : -1]
+#define _UTIL_STATIC_ASSERT_2(COND,L) _UTIL_STATIC_ASSERT_3(COND,AT_LINE_##L)
+#define _UTIL_STATIC_ASSERT_1(COND,L) _UTIL_STATIC_ASSERT_2(COND,L)
+#define UTIL_STATIC_ASSERT(COND) _UTIL_STATIC_ASSERT_1(COND,__LINE__)
+
+
+#endif //UTIL_H
+
