bc9bc2df69b7eafeb9f322e253c3e232cee0f428
sleeptightAnsiC <91839286+sleeptightAnsiC@users.noreply.github.com>
2024-08-05 17:13:32 +0200

    fix: emcc builds in proper directories

--- rcp/build_web.sh
+++ rcp/build_web.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
+# WARN: MUST BE CALLED FROM PROJECT ROOT DIRECTORY!
 
 set -euo pipefail
 
-# WARN: MUST BE CALLED FROM PROJECT ROOT DIRECTORY!
-
-TMP="tmp/emscripten"
-BIN="bin/emscripten"
+TMP="tmp/emcc"
+BIN="bin/emcc"
 SRC="src"
-RAYDIR=./lib/raylib/src
+RCP="rcp"
+RAYSRC=lib/raylib/src
 
 for i in emcc zip; do
 	which $i > /dev/null || exit 1
@@ -16,21 +16,20 @@ done
 mkdir -p $TMP $BIN
 
 # https://github.com/raysan5/raylib/wiki/Working-for-Web-%28HTML5%29#21-command-line-compilation
-pushd $RAYDIR
+pushd $RAYSRC
 make clean
 emcc -c rcore.c -Os -Wall -DPLATFORM_WEB -DGRAPHICS_API_OPENGL_ES2
 emcc -c rshapes.c -Os -Wall -DPLATFORM_WEB -DGRAPHICS_API_OPENGL_ES2
 emcc -c rtextures.c -Os -Wall -DPLATFORM_WEB -DGRAPHICS_API_OPENGL_ES2
 emcc -c rtext.c -Os -Wall -DPLATFORM_WEB -DGRAPHICS_API_OPENGL_ES2
 emcc -c rmodels.c -Os -Wall -DPLATFORM_WEB -DGRAPHICS_API_OPENGL_ES2
-emcc -c utils.c -Os -Wall -DPLATFORM_WEB
-emcc -c raudio.c -Os -Wall -DPLATFORM_WEB
+emcc -c utils.c -Os -Wall -DPLATFORM_WEB -DGRAPHICS_API_OPENGL_ES2
+emcc -c raudio.c -Os -Wall -DPLATFORM_WEB -DGRAPHICS_API_OPENGL_ES2
 popd
 
-mv $RAYDIR/*.o $TMP
+mv $RAYSRC/*.o $TMP
 
-emcc $TMP/*.o src/*.c --shell-file shell.html -s USE_GLFW=3 -s ASYNCIFY -o index.html
+emcc $TMP/*.o $SRC/*.c --shell-file $RCP/shell.html -s USE_GLFW=3 -s ASYNCIFY -o $BIN/index.html
 
-rm *.zip -f
-zip rlspr.zip index*
+zip -u $BIN/rlspr.zip $BIN/index*
 
